787ca9941a4c86f9e78068e001493b8b8c721dab
Omar Ali <omarali@omarali.us>
2012-02-04 15:18:33 -0800

    removed require ruby-debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-    require 'ruby-debug'
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
